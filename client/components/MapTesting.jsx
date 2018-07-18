a8626273854e49fd97495151c13c1bf94a9cb7f4
MichaelMitch <michael.j.mitchelson@gmail.com>
2018-07-19 07:40:47 +1200

    got map rendering

--- client/components/MapTesting.jsx
+++ client/components/MapTesting.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {addWord} from '../actions'
+import L from 'leaflet'
 
-function submitWord (e, dispatch) {
-  if (e.keyCode === 13) {
-    dispatch(addWord(e.currentTarget.value))
-    e.currentTarget.value = ''
-  }
-}
-
-class MapTesting extends React.component {
-  componentDidMount () {
-    this.props.dispatch
+class MapTesting extends React.Component {
+  componentDidMount() {
+    this.map();
   }
 
-}
+  map() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
 
-export default connect()(MapTesting)
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+
+  render() {
+    return <div id="map">xx</div>  
+  }
+}
+export default MapTesting
